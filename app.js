d9d994787b66a7c9e65b8cb0f3cf7ae3d0d500d3
limzeyu <s10257103@connect.np.edu.sg>
2024-10-29 00:34:10 +0800

    added public folder for front end integration with backend

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const sql = require("mssql");
 const dbConfig = require("./dbConfig");
+const userController = require("./controllers/userController");
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-const userController = require("./controllers/userController");
+app.use(express.static('public'));
 
 
 //User
