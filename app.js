5addd8308666a21aeab8d6f51a08c408c5f3a2e0
limzeyu <s10257103@connect.np.edu.sg>
2024-11-01 13:03:54 +0800

    add main page for front end , add parallel execution

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const sql = require("mssql");
+const mongoose = require("mongoose");
 const dbConfig = require("./dbConfig");
 const userController = require("./controllers/userController");
+const dbURI = "mongodb+srv://Chimken:FMGSOzqLy1SegpFI@fsdpassignment.p4h2x.mongodb.net/"
 
 const app = express();
 const port = 3000;
@@ -20,7 +22,8 @@ app.post("/user",userController.createUser);
 // Start the server and connect to the database
 app.listen(port, async () => {
     try {
-      await sql.connect(dbConfig);
+      // await sql.connect(dbConfig);
+      mongoose.connect(dbURI);
       console.log(`Database connected and server running on port ${port}`);
     } catch (err) {
       console.error('Database connection error:', err);
@@ -31,7 +34,8 @@ app.listen(port, async () => {
   // Handle graceful shutdown
   process.on('SIGINT', async () => {
     console.log('Shutting down gracefully...');
-    await sql.close();
+    // await sql.close();
+    mongoose.connection.close();
     process.exit(0);
   });
    
--- models/user.js
+++ models/user.js
@@ -1,46 +1,58 @@
 const sql = require("mssql");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 const dbConfig = require("../dbConfig");
 
 //User class
-class User
-{
-    constructor(userId, name, email, password)
+//Mongoose trying
+const userSchema = new Schema(
     {
-        this.userId = userId;
-        this.name = name;
-        this.email = email;
-        this.password = password;
+        name: { type: String, required: true },
+        email: { type: String, required: true, unique: true }, 
+        password: { type: String, required: true}
     }
+);
+const User = mongoose.model('Users',userSchema)
+
+// class User
+// {
+//     constructor(userId, name, email, password)
+//     {
+//         this.userId = userId;
+//         this.name = name;
+//         this.email = email;
+//         this.password = password;
+//     }
 
 
-    static async getAllUser()
-    {
-        const connection = await sql.connect(dbConfig);
-        const sqlQuery = `SELECT * FROM Users`;
-        const request = connection.request();
-        const result = await request.query(sqlQuery);
-        connection.close();
-        return result.recordset.map
-        (
-            (row) => new User(row.userId, row.name, row.email, row.password)
-        );
-    }
+//     static async getAllUser()
+//     {
+//         const connection = await sql.connect(dbConfig);
+//         const sqlQuery = `SELECT * FROM Users`;
+//         const request = connection.request();
+//         const result = await request.query(sqlQuery);
+//         connection.close();
+//         return result.recordset.map
+//         (
+//             (row) => new User(row.userId, row.name, row.email, row.password)
+//         );
+//     }
 
-    static async createUser(newUserData)
-    {
-        const connection = await sql.connect(dbConfig);
-        const sqlQuery = `INSERT INTO Users (name, email, password) VALUES (@name, @Email, @Password)`
-        const request = connection.request();
-        request.input('name',newUserData.name);
-        request.input('email', newUserData.email);
-        request.input('password',newUserData.password);
-        const result = await request.query(sqlQuery);
-        connection.close();
+//     static async createUser(newUserData)
+//     {
+//         const connection = await sql.connect(dbConfig);
+//         const sqlQuery = `INSERT INTO Users (name, email, password) VALUES (@name, @Email, @Password)`
+//         const request = connection.request();
+//         request.input('name',newUserData.name);
+//         request.input('email', newUserData.email);
+//         request.input('password',newUserData.password);
+//         const result = await request.query(sqlQuery);
+//         connection.close();
         
-        return result.rowsAffected > 0;
-    }
+//         return result.rowsAffected > 0;
+//     }
 
-}
+// }
 
 
 
