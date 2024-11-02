ece259c7222a77edf1e52c98597bda7283b2f493
limzeyu <s10257103@connect.np.edu.sg>
2024-11-02 15:27:36 +0800

    add login page that works with backend and a homepage skeleton

--- models/user.js
+++ models/user.js
@@ -7,7 +7,6 @@ const dbConfig = require("../dbConfig");
 //Mongoose trying
 const userSchema = new Schema(
     {
-        name: { type: String, required: true },
         email: { type: String, required: true, unique: true }, 
         password: { type: String, required: true}
     }
